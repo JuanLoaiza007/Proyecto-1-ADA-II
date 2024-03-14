b2f1221b997a7197b0746740f4f723c7244d1656
Juan David <juan.loaiza.santiago@correounivalle.edu.co>
2024-03-14 08:13:16 -0500

    Initial commit

---
+++ exhaustivo.py
@@ -0,0 +1,93 @@
+from main import *
+
+# Función tIR para calcular el tiempo de inicio de riego
+def tIR(f, pi):
+    
+    def calcularTiempoInicio(tablon):
+        
+        def indiceTablon(pi, actual):
+            
+            def buscarIndice(v, e, indice):
+                if v[0] == e:
+                    return indice
+                else:
+                    return buscarIndice(v[1:], e, indice + 1)
+            return buscarIndice(pi, actual, 0)
+
+        if tablon == pi[0]:
+            return 0
+        else:
+            return calcularTiempoInicio(pi[indiceTablon(pi, tablon) - 1]) + treg(f, pi[indiceTablon(pi, tablon) - 1])
+
+    tiemposInicioRiego = [calcularTiempoInicio(ti) for ti in range(len(f))]
+    return tiemposInicioRiego
+
+# Función costoRiegoTablon para calcular el costo de riego de un tablón
+def costoRiegoTablon(i, f, pi):
+    
+    def costoRiego(ti):
+        if tsup(f, i) - treg(f, i) >= ti:
+            return tsup(f, i) - (ti + treg(f, i))
+        else:
+            return prio(f, i) * ((ti + treg(f, i)) - tsup(f, i))
+
+    return costoRiego(tIR(f, pi)[i])
+
+# Función costoRiegoFinca para calcular el costo de riego de la finca
+def costoRiegoFinca(f, pi):
+    
+    costos = [costoRiegoTablon(i, f, pi) for i in range(len(f))]
+
+    def sumarElementos(vector):
+        if not vector:
+            return 0
+        else:
+            return vector[0] + sumarElementos(vector[1:])
+
+    return sumarElementos(costos)
+
+# Función costoMovilidad para calcular el costo de movilidad en la finca
+def costoMovilidad(f, pi, d):
+    
+    n = len(pi)
+    costos = [d[pi[j]][pi[j + 1]] for j in range(n - 1)]
+
+    def sumarElementos(vector):
+        if not vector:
+            return 0
+        else:
+            return vector[0] + sumarElementos(vector[1:])
+
+    return sumarElementos(costos)
+
+# Función para generar todas las posibles programaciones de riego
+def generarProgramacionesRiego(f):
+    
+    tablones = list(range(len(f)))
+
+    def generar(restantes):
+        if len(restantes) == 1:
+            return [[restantes[0]]]
+        else:
+            permutaciones = []
+            for i in restantes:
+                resto = [x for x in restantes if x != i]
+                for permutacion in generar(resto):
+                    permutaciones.append([i] + permutacion)
+            return permutaciones
+
+    return generar(tablones)
+
+# Función ProgramacionRiegoOptimo para encontrar la programación de riego óptima
+def ProgramacionRiegoOptimo(f, d):
+    
+    progCostos = [(pi, costoRiegoFinca(f, pi) + costoMovilidad(f, pi, d)) for pi in generarProgramacionesRiego(f)]
+
+    def encontrarMinimo(optimaActual, costosRestantes):
+        if not costosRestantes:
+            return optimaActual
+        else:
+            nuevoMin = costosRestantes[0] if costosRestantes[0][1] < optimaActual[1] else optimaActual
+            return encontrarMinimo(nuevoMin, costosRestantes[1:])
+
+    return encontrarMinimo(progCostos[0], progCostos[1:])
---
+++ test_correctitud.py
@@ -0,0 +1,81 @@
+from exhaustivo import *
+
+# Ejemplo 1
+print("Pruebas con Ejemplo 1:")
+
+finca1 = [(10, 3, 4), (5, 3, 3), (2, 2, 1), (8, 1, 1), (6, 4, 2)]
+
+d1 = [
+    [0, 2, 2, 4, 4],
+    [2, 0, 4, 2, 6],
+    [2, 4, 0, 2, 2],
+    [4, 2, 2, 0, 4],
+    [4, 6, 2, 4, 0]
+]
+
+progRiego1 = [0, 1, 2, 3, 4]
+
+print("Tiempo de inicio de riego (Esperado: [0, 3, 6, 8, 9]):", tIR(finca1, progRiego1))
+print("Costo de riego de la finca (Esperado: 31):", costoRiegoFinca(finca1, progRiego1))
+print("Costo de movilidad en la finca (Esperado: 12):", costoMovilidad(finca1, progRiego1, d1))
+print("\n\n")
+
+progRiego2 = [0, 1, 4, 2, 3]
+print("Tiempo de inicio de riego (Esperado: [0, 3, 10, 12, 6]):", tIR(finca1, progRiego2))
+print("Costo de riego de la finca (Esperado: 33):", costoRiegoFinca(finca1, progRiego2))
+print("Costo de movilidad en la finca (Esperado: 12):", costoMovilidad(finca1, progRiego2, d1))
+print("\n\n")
+
+progRiego3 = [2, 1, 4, 3, 0]
+print("Tiempo de inicio de riego (Esperado: [10, 2, 0, 9, 5]):", tIR(finca1, progRiego3))
+print("Costo de riego de la finca (Esperado: 20):", costoRiegoFinca(finca1, progRiego3))
+print("Costo de movilidad en la finca (Esperado: 18):", costoMovilidad(finca1, progRiego3, d1))
+print("\n\n")
+
+
+# Ejemplo 2
+print("Pruebas con Ejemplo 2:")
+
+finca2 = [(9, 3, 4), (5, 3, 3), (2, 2, 1), (8, 1, 1), (6, 4, 2)]
+
+d2 = [
+    [0, 2, 2, 4, 4],
+    [2, 0, 4, 2, 6],
+    [2, 4, 0, 2, 2],
+    [4, 2, 2, 0, 4],
+    [4, 6, 2, 4, 0]
+]
+
+print("Tiempo de inicio de riego (Esperado: [0, 3, 6, 8, 9]):", tIR(finca2, progRiego1))
+print("Costo de riego de la finca (Esperado: 30):", costoRiegoFinca(finca2, progRiego1))
+print("Costo de movilidad en la finca (Esperado: 12):", costoMovilidad(finca2, progRiego1, d2))
+print("\n\n")
+
+progRiego4 = [2, 1, 4, 3, 0]
+print("Tiempo de inicio de riego (Esperado: [10, 2, 0, 9, 5]):", tIR(finca2, progRiego4))
+print("Costo de riego de la finca (Esperado: 24):", costoRiegoFinca(finca2, progRiego4))
+print("Costo de movilidad en la finca (Esperado: 18):", costoMovilidad(finca2, progRiego4, d2))
+print("\n\n")
+
+progRiego5 = [2, 1, 4, 0 ,3]
+print("Tiempo de inicio de riego (Esperado: [9, 2, 0, 12, 5]):", tIR(finca2, progRiego5))
+print("Costo de riego de la finca (Esperado: 23):", costoRiegoFinca(finca2, progRiego5))
+print("Costo de movilidad en la finca (Esperado: 18):", costoMovilidad(finca2, progRiego5, d2))
+print("\n\n")
+
+print("Pruebas de todas las programaciones de riego posibles:")
+
+# Pruebas de todas las programaciones de riego posibles
+def calcular_permutaciones(n):
+    if n <= 1:
+        return 1
+    else:
+        return n * calcular_permutaciones(n - 1)
+
+# Pruebas hasta de 10 tablones
+for i in range(1, 11):
+    f = finca_al_azar(i)
+    print(f"Para {i} tablones:")
+    print("Número de permutaciones generadas:", len(generarProgramacionesRiego(f)))
+    print("Número de permutaciones esperadas:", calcular_permutaciones(i))
+    print("\n\n")

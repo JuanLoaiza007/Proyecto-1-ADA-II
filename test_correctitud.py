a4cd1ca699ad6e443da258dbb447509d77ccbb6c
Juan David <juan.loaiza.santiago@correounivalle.edu.co>
2024-03-19 15:00:44 -0500

    patch 6

--- test_correctitud.py
+++ test_correctitud.py
@@ -4,4 +4,4 @@ from exhaustivo import *
 finca1 = [(10, 3, 4), (5, 3, 3), (2, 2, 1), (8, 1, 1), (6, 4, 2)]
 progRiego1 = [0, 1, 4, 2, 3]
 
-print("Optima es: ", ProgramacionRiegoOptimo(finca1))
+print("Optima es [Esperado: 17]: ", ProgramacionRiegoOptimo(finca1))

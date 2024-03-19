f339a19095cda41ddaa8d4b6f0812d8560393091
Juan David <juan.loaiza.santiago@correounivalle.edu.co>
2024-03-19 14:54:34 -0500

    patch 6

--- exhaustivo.py
+++ exhaustivo.py
@@ -1,40 +1,38 @@
 from main import *
 
 
-def costoRiegoTablon(i, f, pi):
-
-    def calcularTiempoInicio(tablon):
-
-        if tablon == pi[0]:
-            return 0
-        else:
-            return calcularTiempoInicio(pi[pi.index(tablon) - 1]) + treg(f, pi[pi.index(tablon) - 1])
-
-    def costoRiego(ti):
-        if tsup(f, i) - treg(f, i) >= ti:
-            return tsup(f, i) - (ti + treg(f, i))
-        else:
-            return prio(f, i) * ((ti + treg(f, i)) - tsup(f, i))
-
-    tiemposInicioRiego = [calcularTiempoInicio(ti) for ti in range(len(f))]
-
-    return costoRiego(tiemposInicioRiego[i])
-
-
-def costoRiegoFinca(f, pi):
-
-    costos = [costoRiegoTablon(i, f, pi) for i in range(len(f))]
-
-    def sumarElementos(vector):
-        if not vector:
-            return 0
-        else:
-            return vector[0] + sumarElementos(vector[1:])
-
-    return sumarElementos(costos)
-
-
 def ProgramacionRiegoOptimo(f):
+
+    def costoRiegoTablon(i, f, pi):
+
+        def calcularTiempoInicio(tablon):
+
+            if tablon == pi[0]:
+                return 0
+            else:
+                return calcularTiempoInicio(pi[pi.index(tablon) - 1]) + treg(f, pi[pi.index(tablon) - 1])
+
+        def costoRiego(ti):
+            if tsup(f, i) - treg(f, i) >= ti:
+                return tsup(f, i) - (ti + treg(f, i))
+            else:
+                return prio(f, i) * ((ti + treg(f, i)) - tsup(f, i))
+
+        tiemposInicioRiego = [calcularTiempoInicio(ti) for ti in range(len(f))]
+
+        return costoRiego(tiemposInicioRiego[i])
+
+    def costoRiegoFinca(f, pi):
+
+        costos = [costoRiegoTablon(i, f, pi) for i in range(len(f))]
+
+        def sumarElementos(vector):
+            if not vector:
+                return 0
+            else:
+                return vector[0] + sumarElementos(vector[1:])
+
+        return sumarElementos(costos)
 
     tablones = list(range(len(f)))
 

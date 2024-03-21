c42b9b3ba4963f083e97a2c9d356ef577d2fdef2
Juan David <juan.loaiza.santiago@correounivalle.edu.co>
2024-03-21 11:54:46 -0500

    Implementation of the file selector in the txt parser "lector_txt.py"

--- lector_txt.py
+++ lector_txt.py
@@ -1,26 +1,32 @@
-def leer_archivo_txt(nombre_archivo):
-    try:
-        with open(nombre_archivo, "r") as archivo:
-            # Leer la primera línea para obtener el valor de n
-            n = int(archivo.readline().strip())
-            
-            # Leer las siguientes n líneas con los 3 enteros separados por comas
-            datos = []
-            for _ in range(n):
-                linea = archivo.readline().strip()
-                valores = [int(x) for x in linea.split(",")]
-                datos.append(valores)
-            return datos
-        
-    except FileNotFoundError:
-        print(f"El archivo '{nombre_archivo}' no se encontró.")
+# [lector_txt.py]
+from file_selector import File_selector
+
+
+def leer_archivo_txt():
+    nombre_archivo = File_selector.select("data")
+    if nombre_archivo:
+        try:
+            with open(nombre_archivo, "r") as archivo:
+                n = int(archivo.readline().strip())
+
+                datos = []
+                for _ in range(n):
+                    linea = archivo.readline().strip()
+                    valores = tuple([int(x) for x in linea.split(",")])
+                    datos.append(valores)
+                return datos
+
+        except FileNotFoundError:
+            print(f"El archivo '{nombre_archivo}' no se encontró.")
+            return None
+    else:
+        print("No se seleccionó ningún archivo.")
         return None
 
-# Ejemplo de uso
-nombre_archivo = "test.txt"
-datos_leidos = leer_archivo_txt(nombre_archivo)
-if datos_leidos:
-    print(f"Datos leídos del archivo '{nombre_archivo}':")
-    #for linea in datos_leidos:
-     #   print(linea)
-    print(datos_leidos)
+
+if __name__ == '__main__':
+    # Ejemplo de uso
+    datos_leidos = leer_archivo_txt()
+    if datos_leidos:
+        print("Datos leídos del archivo:")
+        print(datos_leidos)

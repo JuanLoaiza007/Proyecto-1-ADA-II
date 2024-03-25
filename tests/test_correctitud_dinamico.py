a7f163ddcd83a471640e743978bb44de6b49fc2d
Sebastian Muñoz <juan.munoz.rojas@correounivalle.edu.co>
2024-03-25 12:32:01 -0500

    Primer bosquejo de implementacion de programación dinamica

--- tests/test_correctitud_dinamico.py
+++ tests/test_correctitud_dinamico.py
@@ -1,7 +1,9 @@
-from models.exhaustivo import *
+# from models.exhaustivo import *
+from models.dinamico import *
 from models.tools.lector_txt import leer_archivo_txt
 
-class test_correctitud:
+
+class test_correctitud_dinamico:
     def start():
         # Ejemplo 1
         finca1 = [(2, 2, 1), (3, 4, 1), (4, 2, 1), (7, 6, 1)]
@@ -9,13 +11,7 @@ class test_correctitud:
         finca3 = [(4, 4, 1), (8, 4, 1), (16, 8, 1), (33, 16, 1)]
         finca4 = [(10, 3, 4), (5, 3, 3), (2, 2, 1), (6, 4, 2)]
 
-        # tsup - (t_ini + treg)             ,si tsup - treg >= t_ini
-        #   8  - (  5   +   3)              ,si  8   -  3   >=   5
-
-        # Cuando t_ini = tsup-treg costoRiegoTablon = 0clea
-
-        # Cuando
-        
         finca = leer_archivo_txt()
-
-        print("Optima es: ", roFB(finca))
+        # finca = finca4
+        print("Solucion dinamica", roPD(finca), "\n\n")
+        # print("Solucion exhaustiva", roFB(finca))
